67376f3a5e06ee7660e8481009f2ee57d3bba255
李龙 <lilong2576@hanweb.com>
2025-03-18 17:10:55 +0800

    feat: 自定义对于未定义的 HTTP方法请求的错误

--- src/routes/api/add/+server.js
+++ src/routes/api/add/+server.js
@@ -1,6 +1,11 @@
-import { json } from "@sveltejs/kit";
+import { error, json, text } from "@sveltejs/kit";
 
 export async function POST({ request }) {
   const { a, b } = await request.json();
   return json(a + b);
+}
+
+
+export async function fallback({ request }) {
+  return text(`I caught your ${request.method} request!`);
 }
